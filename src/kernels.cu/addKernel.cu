85121aee30cff8292da6d6607ad7819cdf732f15
GalBG2003 <galgershon8@gmail.com>
2026-03-11 12:40:49 -0400

    every threads preform two computations

--- src/kernels.cu/addKernel.cu
+++ src/kernels.cu/addKernel.cu
@@ -4,15 +4,19 @@
 __global__ void addKernel(float *c, const float *a, const float *b, unsigned int vectorSize)
 {
     int i = threadIdx.x + blockDim.x * blockIdx.x;
-    if(i < vectorSize )
+    if(i < vectorSize/2 ){
         c[i] = ((i & 1) == 0) * b[i] * b[i] + ((i & 1) == 1) * (a[i] + b[i]);
+        c[i + vectorSize/2] = ((i & 1) == 0) * b[i + vectorSize/2] * b[i + vectorSize/2] + (((i + vectorSize/2) & 1) == 1) * (a[i + vectorSize/2] + b[i + vectorSize/2]);
+    }
+        
+    
 }
 
 cudaError_t addWithCuda(float *c, const float *a, const float *b, unsigned int size,unsigned int ThreadsPerBlock)
 {
     float *dev_a = 0, *dev_b = 0, *dev_c = 0;
     cudaError_t cudaStatus;
-    int numBlocks = (size + ThreadsPerBlock - 1)/ThreadsPerBlock;
+    int numBlocks = (size/2 + ThreadsPerBlock - 1)/ThreadsPerBlock;
     cudaStatus = cudaSetDevice(0);
 
     cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(float));
